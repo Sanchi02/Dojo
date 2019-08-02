e99df79bc66639d105dfcd850c756ec78ea95e0c
sanchi002 <sanchibadkas@gmail.com>
2019-08-02 17:40:48 +0530

    Reverse LL

--- Basics/BasicSinglyLL.py
+++ Basics/BasicSinglyLL.py
@@ -23,11 +23,48 @@ class SinglyLL:
         else:
             tempNode.next = self.head
             self.head = tempNode
+            
+    def sizeOfList(self):
+        currNode = self.head
+        num = 1
+        while(currNode.next):
+            currNode = currNode.next
+            num +=1
+        return num
+            
+    def insertAtLoc(self,data, position):
+        if(position==0):
+            self.insertAtHead(data)
+        if(position>self.sizeOfList()+1):
+            print("Number out of range")
+            return
+        currNode = self.head
+        for i in range(2,position):
+            currNode = currNode.next
+        nextNode = currNode.next
+        tempNode = Node(data)
+        currNode.next = tempNode
+        tempNode.next = nextNode
         
+    def reverseList(self): 
+        prevNode = None
+        currNode = self.head 
+        while(currNode is not None): 
+            nextNode = currNode.next
+            currNode.next = prevNode 
+            prevNode = currNode
+            currNode = nextNode
+        self.head = prevNode
+                 
 listy = SinglyLL()
 
 listy.insertAtHead(90)
 listy.insertAtHead(46)
 listy.insertAtHead(78)
+listy.insertAtLoc(45,4)
+listy.printList()
+print("Reversing")
+listy.reverseList()
+# listy.sizeOfList()
 listy.printList()
     
