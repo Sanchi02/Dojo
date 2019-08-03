5a28dc694992d4089d150d1c4595b5b6eafd4197
sanchi002 <sanchibadkas@gmail.com>
2019-08-03 15:53:00 +0530

    TLE exists to be modified

---
+++ IPCTRAIN.py
@@ -0,0 +1,23 @@
+# https://www.codechef.com/JULY17/problems/IPCTRAIN
+
+import heapq as h
+import math
+
+for t in range(int(input())):
+    numTrainers, campDays = map(int, input().split(' '))
+    listy = []
+    priorityHeap = []
+    for i in range(numTrainers):
+        di, ti, si = map(int, input().split(' '))
+        listy.append([di,ti,si])
+    listy = sorted(listy,key=lambda x:x[0])
+    for day in range(1,campDays+1):
+        while(len(listy) !=0 and listy[0][0]==day):
+            for tmp in range(listy[0][1]):
+                test = -1*listy[0][2]
+                h.heappush(priorityHeap,test)
+            listy.pop(0)
+        if(len(priorityHeap)!=0):
+            h.heappop(priorityHeap)
+    print(abs(sum(priorityHeap)))
+        
